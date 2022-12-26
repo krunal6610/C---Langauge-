fbde3b55f06d28bc42b1b1a59890e5ba2949ec5f
krunal6610 <120013579+krunal6610@users.noreply.github.com>
2022-12-26 10:04:05 +0530

    Add files via upload

---
+++ PRINT_MESSEGE.cpp
@@ -0,0 +1,16 @@
+/*
+
+Program to print Message " Welcome to Tops Technologies "
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to Tops Technologies";
+
+    return 0;
+}
